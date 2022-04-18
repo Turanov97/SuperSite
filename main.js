f3d6751108471447851512c72c26bf86ed7e0505
turanov97 <turanovbekzod116@gmail.com>
2022-04-18 18:07:01 +0500

    responsiv 1

--- main.js
+++ main.js
@@ -5,11 +5,11 @@ $("#nav_toggle").on("click", function () {
   $(".overlay").toggleClass("active");
 });
 
-$('.menu_close').on('click', function ()  {
+$(".menu_close").on("click", function () {
   $("#menu").removeClass("menu__active");
   $("#nav_toggle").removeClass("active");
   $(".overlay").removeClass("active");
-})
+});
 
 const overlay = document.querySelector(".overlay");
 
@@ -37,13 +37,17 @@ $(".searchBtn").click(function () {
 
 /* SLIDER */
 var swiper = new Swiper(".mySwiper", {
-  speed: 6000,
-  spaceBetween: 0,
+  speed: 10000,
+  spaceBetween: 20,
   autoplay: {
-    delay: 0,
-    disableOnInteraction: false,
+    delay: 100,
   },
+  loop: true,
   breakpoints: {
+    450: {
+      slidesPerView: 1.5,
+      spaceBetween: 20,
+    },
     640: {
       slidesPerView: 2,
       spaceBetween: 20,
@@ -83,34 +87,50 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 
-
 /*selext to ona est  */
 
-document.querySelectorAll(".select-wrap").forEach(function(wrap) {
+document.querySelectorAll(".select-wrap").forEach(function (wrap) {
   let select = wrap.querySelector(".custom-select");
   let classes = select.getAttribute("class"),
-      id      = select.getAttribute("id"),
-      name    = select.getAttribute("name");
-  let template =  '<div class="' + classes + '">';
-      template += '<span class="custom-select-trigger">' + select.getAttribute("placeholder") + '</span>';
-      template += '<div class="custom-options">';
-      select.querySelectorAll("option").forEach(function(option) {
-        template += '<span class="custom-option' + '"data-value="' + option.getAttribute("value") + '">' + option.innerText + '</span>';
-      });
-  template += '</div></div>';
-  let select_wrapper = document.createElement('div'); 
-  select.style.display = 'none';
+    id = select.getAttribute("id"),
+    name = select.getAttribute("name");
+  let template = '<div class="' + classes + '">';
+  template +=
+    '<span class="custom-select-trigger">' +
+    select.getAttribute("placeholder") +
+    "</span>";
+  template += '<div class="custom-options">';
+  select.querySelectorAll("option").forEach(function (option) {
+    template +=
+      '<span class="custom-option' +
+      '"data-value="' +
+      option.getAttribute("value") +
+      '">' +
+      option.innerText +
+      "</span>";
+  });
+  template += "</div></div>";
+  let select_wrapper = document.createElement("div");
+  select.style.display = "none";
   select_wrapper.innerHTML = `<div class="custom-select-wrapper">${template}</div>`;
   wrap.appendChild(select_wrapper);
 });
 
-document.querySelector(".custom-select-trigger").addEventListener("click", function() {
-  document.querySelector(".custom-select-trigger").closest(".custom-select").classList.toggle("opened");
-});
-document.querySelectorAll(".custom-option").forEach((option)=>{
-  option.addEventListener("click", function() {
-    option.closest(".select-wrap").querySelector("select").value = option.getAttribute("data-value");
+document
+  .querySelector(".custom-select-trigger")
+  .addEventListener("click", function () {
+    document
+      .querySelector(".custom-select-trigger")
+      .closest(".custom-select")
+      .classList.toggle("opened");
+  });
+document.querySelectorAll(".custom-option").forEach((option) => {
+  option.addEventListener("click", function () {
+    option.closest(".select-wrap").querySelector("select").value =
+      option.getAttribute("data-value");
     option.closest(".custom-select").classList.remove("opened");
-    option.closest(".custom-select").querySelector(".custom-select-trigger").innerText = option.innerText;
+    option
+      .closest(".custom-select")
+      .querySelector(".custom-select-trigger").innerText = option.innerText;
   });
 });
